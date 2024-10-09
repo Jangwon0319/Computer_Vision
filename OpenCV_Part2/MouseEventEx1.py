697b36b251f026316ab55abbff9168d861ca04af
Jangwon0319 <ryujw0319@gmail.com>
2024-10-09 23:40:21 +0900

    OpenCV_Part2 실습

---
+++ OpenCV_Part2/MouseEventEx1.py
@@ -0,0 +1,33 @@
+import cv2
+
+def on_mouse(event, x, y, flags, param):
+    global oldx, oldy
+
+    if event == cv2.EVENT_LBUTTONDOWN:
+        oldx, oldy = x, y
+        print('EVENT_LBUTTONDOWN: %d, %d' % (x,y))
+    
+    elif event == cv2.EVENT_LBUTTONUP:
+        print('EVENT_LBUTTONUP: %d, %d' % (x,y))
+
+    elif event == cv2.EVENT_MOUSEMOVE:
+        if flags & cv2.EVENT_FLAG_LBUTTON:
+            cv2.line(img, (oldx, oldy), (x, y), (0, 255, 255), 2)
+            cv2.imshow('img', img)
+            oldx, oldy = x, y
+
+img = cv2.imread('OpenCV_Part2/lenna.bmp')
+
+if img is None:
+    exit()
+
+cv2.namedWindow('img')
+cv2.setMouseCallback('img', on_mouse)
+
+cv2.imshow('img', img)
+cv2.waitKey()
+cv2.destroyAllWindows()
+
+
+
+    
