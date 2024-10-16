9c1e97a77e5bd4310c55e9287e264c8bbf7f1310
Jangwon0319 <ryujw0319@gmail.com>
2024-10-17 00:14:38 +0900

    Geomatric Transformation 실습

---
+++ Geometric Transforamtion/Rotation.py
@@ -0,0 +1,17 @@
+import numpy as np
+import cv2
+
+def affine_rotation():
+    src = cv2.imread('lenna.bmp')
+
+    cp = (src.shape[1] / 2, src.shape[0] / 2)
+    affine_mat = cv2.getRotationMatrix2D(cp, 20, 1)
+
+    dst = cv2.warpAffine(src, affine_mat, (0,0))
+
+    cv2.imshow('src', src)
+    cv2.imshow('dst', dst)
+    cv2.waitKey()
+    cv2.destroyAllWindows()
+
+affine_rotation()
