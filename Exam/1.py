3dd52ab90916657eced087864ed0440862915d7f
Jangwon0319 <ryujw0319@gmail.com>
2024-10-18 20:36:18 +0900

    Numpy 실습

--- Exam/1.py
+++ Exam/1.py
@@ -3,10 +3,7 @@ import numpy as np
 nums = np.array([[[1, 4, 2], [5, 6, 4]], 
                  [[7, 5, 3], [8, 2, 9]]])
 
-arr = np.array([[[1, 4], [5, 6], [2, 4]],
-                [[7, 5], [5, 6], [8, 9]]])
-
-
+arr=np.transpose(nums,(0,2,1))
 
 
 print(arr.shape, arr[1,1,0])
